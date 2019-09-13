61f2bc9a4fae94b3114f895a1a2847bc4d79b1fd
Fabian Claar <fabian@awsmdev.de>
2019-09-13 11:50:22 +0200

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'unifi-controller adapter settings': {
+        'en': 'Adapter settings for unifi-controller',
+        'de': 'Adaptereinstellungen für unifi-controller',
+        'ru': 'Настройки адаптера для unifi-controller',
+        'pt': 'Configurações do adaptador para unifi-controller',
+        'nl': 'Adapterinstellingen voor unifi-controller',
+        'fr': "Paramètres d'adaptateur pour unifi-controller",
+        'it': "Impostazioni dell'adattatore per unifi-controller",
+        'es': 'Ajustes del adaptador para unifi-controller',
+        'pl': 'Ustawienia adaptera dla unifi-controller',
+        'zh-cn': 'unifi-controller的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
